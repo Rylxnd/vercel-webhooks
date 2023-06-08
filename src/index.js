64a7becfb0726ea5c27bfd94df5b4f80db7d8efb
Collin <61126081+Collin222@users.noreply.github.com>
2023-06-08 14:49:47 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+const express = require('express');
+const { verifySignature, sendDiscordWebhook } = require('./utils');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/webhooks', async (req, res) => {
+	if (!req.headers['x-vercel-signature']) return res.sendStatus(401);
+	if (!verifySignature(res)) return res.sendStatus(403);
+
+	const payload = req.body.payload;
+
+	const embed = {
+		title: '',
+		description: '',
+		footer: { text: `Deployment ID: ${payload.id}` },
+		timestamp: new Date().toISOString(),
+		url: payload.deployment.url,
+		fields: [
+			{
+				name: 'Project',
+				value: payload.deployment.name,
+				inline: true,
+			},
+			{
+				name: 'Target',
+				value: payload.target,
+				inline: true,
+			},
+		],
+	};
+
+	switch (req.body.type) {
+		case 'deployment.created':
+			embed.title = 'Deployment Created';
+			embed.color = 5090295;
+			break;
+		case 'deployment.succeeded':
+			embed.title = 'Deployment Succeeded';
+			embed.color = 3725737;
+			break;
+		case 'deployment.error':
+			embed.title = 'Deployment Error';
+			embed.color = 16746375;
+			break;
+		case 'deployment.canceled':
+			embed.title = 'Deployment Cancelled';
+			embed.color = 16766011;
+			break;
+		default:
+			return res.sendStatus(201);
+	}
+
+	await sendDiscordWebhook({ embeds: [embed] });
+
+	res.sendStatus(201);
+});
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
---
+++ src/utils.js
@@ -0,0 +1,28 @@
+const crypto = require('crypto');
+const fetch = require('node-fetch');
+
+/**
+ * Verifies the Vercel signature on a request
+ * @param req The request to verify
+ * @returns Whether the signature is valid
+ */
+export const verifySignature = req => {
+	const payload = req.body.payload;
+	const signature = crypto
+		.createHmac('sha1', process.env.VERCEL_SECRET)
+		.update(payload)
+		.digest('hex');
+	return signature === req.headers['x-vercel-signature'];
+};
+
+/**
+ * Sends a discord webhook
+ * @param body The request body
+ * @returns The fetch response
+ */
+export const sendDiscordWebhook = async body => {
+	return await fetch(process.env.DISCORD_WEBHOOK, {
+		method: 'POST',
+		body: JSON.stringify(data),
+	});
+};
