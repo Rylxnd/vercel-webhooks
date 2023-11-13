ed807cf06767fa62a82c81576ae994346483ede7
0xRaptor <personal@lucasgellert.co.uk>
2023-11-13 16:58:39 +0000

    remove debug

--- src/trello.js
+++ src/trello.js
@@ -4,12 +4,8 @@ const { verifyTrelloSignature, sendDiscordWebhook } = require('./utils');
 
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    res.sendStatus(200)
-})
 
 router.post('/', async (req, res) => {
-    res.sendStatus(200)
     if (!req.headers['x-trello-webhook']) return res.sendStatus(401);
     if (!(await verifyTrelloSignature(req.body, req))) return res.sendStatus(403);
 
