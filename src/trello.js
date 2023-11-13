d8bb828f6bbd715f2f1ca88f86e14d6f97a986f5
0xRaptor <personal@lucasgellert.co.uk>
2023-11-13 16:36:28 +0000

    temp fix?

--- src/trello.js
+++ src/trello.js
@@ -5,6 +5,7 @@ const { verifyTrelloSignature, sendDiscordWebhook } = require('./utils');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
+    res.sendStatus(200)
     if (!req.headers['x-trello-webhook']) return res.sendStatus(401);
     if (!(await verifyTrelloSignature(req.body, req))) return res.sendStatus(403);
 
