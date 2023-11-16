f8d33ca2e74063008238f25d07b6f848b77c350f
Rylxnd <rywoodsworth@gmail.com>
2023-11-15 20:04:23 -0500

    trust reverse proxy

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
+app.enable('trust proxy'); // for dev use
+
 app.use('/webhooks/vercel', vercel);
 
 app.use('/webhooks/trello', trello);
